c1ce556584e16273e7bf805b6b5f80a5de60b6d1
Lucas-Cayres-Porto <lucas.porto.tech@gmail.com>
2025-10-17 23:39:10 -0300

    refactor(ciew): arruando a view para mostrar todas as empresas

--- sql/scriptView.sql
+++ sql/scriptView.sql
@@ -1,25 +1,30 @@
 create view informacoesHome as
 select
 	e.nome,
-	f.id_empresa ,
+	e.id "id_empresa",
 	f.tamanho_frota,
 	count(distinct m.status) filter (
-	where m.status = 'A') "ativos",
+where
+	m.status = 'A') "ativos",
 	count(distinct m.status) filter (
-	where m.status = 'I') "inativos",
+where
+	m.status = 'I') "inativos",
 	count(distinct m.status) filter (
-	where m.status = 'M') "manutencao",
+where
+	m.status = 'M') "manutencao",
 	count(distinct et.dt_entrega) filter (
-	where et.dt_entrega<current_date ) "atrasadas",
+where
+	et.dt_entrega<current_date ) "atrasadas",
 	count(distinct et.dt_entrega) filter (
-	where et.dt_entrega >= current_date ) "pendente",
+where
+	et.dt_entrega >= current_date ) "pendente",
 	count(distinct et.cod_entrega) "qt_entrega"
 from
-	frota f
+	empresa e
+left join frota f on
+	f.id_empresa = e.id
 left join caminhao m on
 	f.id = m.id_frota
-left join empresa e on
-	f.id_empresa = e.id
 left join motorista mt on
 	mt.id_empresa = e.id
 left join entrega et on
@@ -27,4 +32,5 @@ left join entrega et on
 group by
 	f.id_empresa,
 	e.nome,
+	e.id,
 	f.tamanho_frota;
